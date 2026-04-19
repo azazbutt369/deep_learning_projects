58c8300bdc379e931e408f2240a214ed97638210
azazbutt369 <azazbutt369@gmail.com>
2026-04-19 14:31:04 +0500

    Add logger utility for logging messages

---
+++ brain_tumour_classification/src/utils/logger.py
@@ -0,0 +1,17 @@
+# src/utils/logger.py
+
+import logging
+
+def get_logger(name="experiment"):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        ch = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(message)s"
+        )
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
+    return logger
