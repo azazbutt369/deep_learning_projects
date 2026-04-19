a24febe2c01b21fa7c6bae484100aec273ccdc8d
azazbutt369 <azazbutt369@gmail.com>
2026-04-19 12:43:00 +0500

    Add EfficientNetB4 model building function

---
+++ brain_tumour_classification/src/models/effnetb4.py
@@ -0,0 +1,26 @@
+# src/models/effnetb4.py
+
+from tensorflow.keras.applications import EfficientNetB4
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
+from tensorflow.keras.models import Model
+
+
+def build_effnetb4(input_shape=(150, 150, 3), num_classes=4):
+    base_model = EfficientNetB4(
+        weights='imagenet',
+        include_top=False,
+        input_shape=input_shape
+    )
+
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    output = Dense(num_classes, activation='softmax')(x)
+
+    model = Model(inputs=base_model.input, outputs=output)
+
+    return model
